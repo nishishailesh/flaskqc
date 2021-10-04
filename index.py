2f2efefe008b7206891c2c44b0427d639120649a
Your Name <you@example.com>
2021-10-04 10:11:26 +0530

    new file:   templates/login.html 	new file:   templates/show_result.html

--- index.py
+++ index.py
@@ -1,11 +1,20 @@
 #!/usr/bin/python3
 from flask import Flask
+from flask import render_template
+from flask_login import LoginManager
+from flask import request
+
 import logging
 import MySQLdb 
 app = Flask(__name__)
 
-from werkzeug.debug import DebuggedApplication
-application = DebuggedApplication(app, True)
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.get(user_id)
 
 
 class my_sql(object):
@@ -36,36 +45,59 @@ class my_sql(object):
   def close_link(self):
     self.con.close()
 
-
+'''
 @app.route('/')
+'''
+
+'''
+To understand decorators
+'''
+'''
 def helloo():
   m=my_sql()
-  m.get_link("127.0.0.1","main_user","main_pass","cl_general")
-  m.run_query("select * from examination",())
+  #m.get_link("127.0.0.1","main_user","main_pass","cl_general")
+  m.get_link("127.0.0.1","rootuser","rootuser","cl_general")
+  m.run_query("select * from result",())
   m.get_single_row()
   return ' First Data:{}'.format(m.data)
 
+app.route('/')(helloo)
+'''
 
+@app.route('/')
+def login():
+  return render_template('login.html')
+
+@app.route('/home',methods=['GET', 'POST'])
+def home_page():
+  data = request.form
+  return render_template('home.html',data=data)
+  
 @app.route('/all')
 def helloo_all():
   m=my_sql()
-  m.get_link("127.0.0.1","main_user","main_pass","cl_general")
-  m.run_query("select * from examination limit 30",())
+  #m.get_link("127.0.0.1","main_user","main_pass","cl_general")
+  m.get_link("127.0.0.1","rootuser","rootuser","cl_general")
+  m.run_query("select * from result limit 30",())
   m.get_single_row()
   all_data=()
   while(m.data):
     all_data=all_data+(m.data,)
     m.get_single_row()  
-  return 'All Data {}'.format(all_data)
+  #return 'All Data {}'.format(all_data)
+  return render_template('show_result.html',all_data=all_data)
 
-@app.route('/<examination_id>')
-def helloo_specific(examination_id):
+
+@app.route('/<int:sample_id>',methods=['GET', 'POST'])
+def helloo_specific(sample_id):
   m=my_sql()
-  m.get_link("127.0.0.1","main_user","main_pass","cl_general")
-  m.run_query("select * from examination where examination_id=%s",(examination_id,))
+  #m.get_link("127.0.0.1","main_user","main_pass","cl_general")
+  m.get_link("127.0.0.1","rootuser","rootuser","cl_general")
+  m.run_query("select * from result where sample_id=%s",(sample_id,))
   m.get_single_row()
   all_data=()
   while(m.data):
     all_data=all_data+(m.data,)
     m.get_single_row()  
-  return 'Examination ID={}: Data {}'.format(examination_id,all_data)
+  #return 'Examination IDDDD={}: Data {}'.format(examination_id,all_data)
+  return render_template('show_result.html',all_data=all_data)
--- index.wsgi
+++ index.wsgi
@@ -1,3 +1,4 @@
 import sys
-sys.path.insert(0,"/usr/share/nchs/flaskqc")
+#sys.path.insert(0,"/usr/share/nchs/flaskqc")
+sys.path.insert(0,"/opt/flaskqc")
 from index import app as application
